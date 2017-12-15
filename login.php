71a325447bdebbaac0e61681efc1c8b920704241
thanhtu <hthanhtu96@gmail.com>
2017-12-14 16:13:25 -0800

    SV C sua file login.php

--- login.php
+++ login.php
@@ -103,14 +103,3 @@ if(isset($_POST['sm']))
   </form>
 </div>
 
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
